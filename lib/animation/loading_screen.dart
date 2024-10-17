3ba9ff0cad3d5e180bf0f3ee0b62b1830e8813b5
Rojan Parajuli <rojanparajuli13@gmail.com>
2024-10-17 14:09:03 +0545

    initial commit

---
+++ lib/animation/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:finance/animation/widget/shimmer_loading.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 1,
+          ),
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return const ShimmerLoadingCard();
+          },
+        ),
+      ),
+    );
+  }
+}
